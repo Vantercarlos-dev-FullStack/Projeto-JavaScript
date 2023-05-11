31b88a12065884f9f0eb601f0694cab649a438e5
Caitano <vantercarlos.caitano@capgemini.com>
2023-05-11 00:11:50 -0300

    Comandos de entrada Javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+var nome = prompt("Qual é o seu Nome ?")
+var sobrenome = prompt("Qual é o seu Sobrenome ?")
+var nomeCompleto = nome + ' ' + sobrenome
+
+document.write("<h1>Seja bem vindo! "+ nomeCompleto +"</h1>")
